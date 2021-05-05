3e490b8db4010cec21670f93f8d0d869a34027ea
BirdManCoding <vogel-julian@web.de>
2021-05-05 18:46:18 +0200

    actionTypes

--- src/actions/postAction.js
+++ src/actions/postAction.js
@@ -1,10 +1,11 @@
 import axios from "axios";
+import { CREATE_POST, FETCH_ALL_POSTS } from "../actions/actionTypes";
 
 export function fetchAllPosts() {
   return async function (dispatch) {
     const { data } = await axios.get(
       "https://jsonplaceholder.typicode.com/posts"
     );
-    dispatch({ type: "FETCH_ALL_POSTS", payload: data });
+    dispatch({ type: FETCH_ALL_POSTS, payload: data });
   };
 }
--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -1,10 +1,10 @@
-const initialPostState = [];
+import { CREATE_POST, FETCH_ALL_POSTS } from "../actions/actionTypes";
 
-const reducer = (state = initialPostState, action) => {
+const reducer = (state = [], action) => {
   switch (action.type) {
-    case "FETCH_ALL_POSTS":
+    case FETCH_ALL_POSTS:
       return action.payload;
-    case "CREATE_POST":
+    case CREATE_POST:
       return state;
     default:
       return state;
